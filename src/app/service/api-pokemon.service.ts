31df5b8d145db0a0f86e90c30a32983f34cb1842
Keoma Silva <keomakds@hotmail.com>
2023-08-11 18:30:00 -0300

    feat: add types

--- src/app/service/api-pokemon.service.ts
+++ src/app/service/api-pokemon.service.ts
@@ -5,16 +5,37 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiPokemonService {
-  pokemons = [];
+  pokemons: any = [];
+  types = [];
+  
 
   constructor( private httpClient:HttpClient) {
     this.getPokemons();
    }
   async getPokemons() {
-    const request = await this.httpClient.get<any>('https://pokeapi.co/api/v2/pokemon?limit=1010').toPromise();
+    const request = await this.httpClient.get<any>('https://pokeapi.co/api/v2/pokemon?limit=9').toPromise();
     
     this.pokemons = request.results;
+    // console.log(request);
+    this.getTypes();
+  }
 
+  async getTypes() {
+    this.pokemons.map( async (element:any) => {
+      console.log(element);
+      const typesPokemon = await this.httpClient.get<any>(`https://pokeapi.co/api/v2/pokemon/${element.name}`).toPromise();
+      element.types = typesPokemon.types;
+      element.batata = typesPokemon.weight;
+      console.log(typesPokemon);
+      
+    });
+    console.log(this.pokemons);
+    
+    
+    // this.types = typesPokemon.results;
+    
+    
+    
   }
 }
  
